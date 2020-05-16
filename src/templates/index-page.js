1c48e8b59ae06bf24645d480809b309592a7adbe
jepar13 <jepar13@student.monash.edu>
2020-05-16 22:56:51 +1000

    yarn

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+//import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 //import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 export const IndexPageTemplate = ({
   image,
@@ -19,11 +20,13 @@ export const IndexPageTemplate = ({
     <div
       className="full-width-image margin-top-0"
       style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
+
+          backgroundImage: `url(${
+            !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+          })`,
+          backgroundPosition: `top left`,
+          backgroundAttachment: `fixed`,
+
       }}
     >
       <div
@@ -55,7 +58,7 @@ export const IndexPageTemplate = ({
           <div className="columns">
             <div className="column is-10 is-offset-1">
               <div className="content">
-              {/*First heading w desc*/}
+              {/*First heading w desc
               <div className="columns">
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
@@ -63,7 +66,7 @@ export const IndexPageTemplate = ({
                   </h3>
                   <p>{description}</p>
                 </div>
-              </div>
+              </div>*/}
 
                 {/*<Features gridItems={intro.blurbs} />
                 <div className="columns">
@@ -80,9 +83,6 @@ export const IndexPageTemplate = ({
                   </h3>
                   <BlogRoll />
                   <div className="column is-1 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
                   </div>
                 </div>
               </div>
@@ -148,25 +148,7 @@ export const pageQuery = graphql`
         }
         heading
         subheading
-        mainpitch {
-          title
-          description
-        }
         description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
       }
     }
   }
